52e383c7d38576ce05817259687928cd0c595de6
SpectatorQL <hlcombine@gmail.com>
2018-10-17 15:23:01 +0200

    Made each player have their own camera Added spawn points

--- Assets/Scripts/MouseCamLook.cs
+++ Assets/Scripts/MouseCamLook.cs
@@ -1,46 +1,45 @@
-﻿/* 
- * author : jiankaiwang
- * description : The script provides you with basic operations 
- *               of first personal camera look on mouse moving.
- * platform : Unity
- * date : 2017/12
- */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class MouseCamLook : MonoBehaviour
+namespace RolePlayOverlord
 {
-    float _yaw;
-    float _pitch;
-    public float Sensitivity = 2.0f;
-
-    Transform _characterHead;
-
-    void Start() 
+    public class MouseCamLook : NetworkBehaviour
     {
-        //Cursor.lockState = CursorLockMode.Locked;
+        float _yaw;
+        float _pitch;
+        public float Sensitivity = 2.0f;
 
-        _yaw = transform.eulerAngles.y;
-        _pitch = transform.eulerAngles.x;
+        [ClientCallback]
+        void Start()
+        {
+            if(isLocalPlayer)
+            {
+                _yaw = transform.eulerAngles.y;
+                _pitch = transform.eulerAngles.x;
+                GetComponent<Camera>().enabled = true;
+            }
+        }
 
-        _characterHead = transform.parent;
-    }
-	
-	void Update()
-    {
-        float rotMin = -30.0f;
-        float rotMax = 30.0f;
-        float inputX = Input.GetAxisRaw("Mouse X");
-        float inputY = Input.GetAxisRaw("Mouse Y");
+        [ClientCallback]
+        void Update()
+        {
+            if(isLocalPlayer)
+            {
+                float rotMin = -30.0f;
+                float rotMax = 30.0f;
+                float inputX = Input.GetAxisRaw("Mouse X");
+                float inputY = Input.GetAxisRaw("Mouse Y");
 
-        _yaw += inputX * Sensitivity;
-        _pitch -= inputY * Sensitivity;
-        _yaw = Mathf.Clamp(_yaw, rotMin, rotMax);
-        _pitch = Mathf.Clamp(_pitch, rotMin, rotMax);
+                _yaw += inputX * Sensitivity;
+                _pitch -= inputY * Sensitivity;
+                _yaw = Mathf.Clamp(_yaw, rotMin, rotMax);
+                _pitch = Mathf.Clamp(_pitch, rotMin, rotMax);
 
-        Vector3 rotation = new Vector3(_pitch, _yaw, 0.0f);
-        _characterHead.eulerAngles = rotation;
+                Vector3 rotation = new Vector3(_pitch, _yaw, 0.0f);
+                transform.eulerAngles = rotation;
+            }
+        }
     }
 }
--- Assets/Scripts/Runtime_Test.cs
+++ Assets/Scripts/Runtime_Test.cs
@@ -3,36 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace RolePlayOverlord
 {
     /*
         NOTE(SpectatorQL): This is all test code for now.
     */
-    public class Runtime_Test : MonoBehaviour
+    public class Runtime_Test : NetworkBehaviour
     {
-        Wall[] _walls;
         static string _dataPath;
         static string _modPath;
         // TODO: This is static for now. Change it once we implement
         // dynamic asset loading?
         static Dictionary<string, Texture2D> _textureCache = new Dictionary<string, Texture2D>();
+
+        Wall[] _walls;
         float _delta;
 
-        [RuntimeInitializeOnLoadMethod]
-        public static void GameMain()
-        {
-            InitializeGame();
-
-            Type[] components =
-            {
-                typeof(Runtime_Test)
-            };
-            GameObject go = new GameObject("Runtime_Test", components);
-            go.transform.position = new Vector3(-1.5f, 0.5f, -0.5f);
-        }
-
-        static void InitializeGame()
+        void InitializeGame()
         {
             int targetFrames = 60;
             Application.targetFrameRate = targetFrames;
@@ -43,6 +32,20 @@ namespace RolePlayOverlord
 #else
             _dataPath = "file:///Test/";
 #endif
+        }
+
+        void Awake()
+        {
+            InitializeGame();
+
+            Type[] components =
+            {
+                typeof(Runtime_Test),
+                typeof(Camera),
+                typeof(MouseCamLook)
+            };
+            GameObject go = new GameObject("Runtime_Test", components);
+            go.transform.position = new Vector3(0.0f, 7.0f, 0.0f);
         }
 
         void Start()
@@ -56,69 +59,6 @@ namespace RolePlayOverlord
             return result;
         }
 
-        void Update()
-        {
-            string keyString = Input.inputString;
-            switch(keyString)
-            {
-                case "1":
-                {
-                    int textureWidth = 200;
-                    int textureHeight = 200;
-                    string texName = "test.png";
-                    string texUrl = GetAssetFilePath(texName);
-
-                    Texture2D tex;
-                    if(_textureCache.ContainsKey(texUrl))
-                    {
-                        tex = _textureCache[texUrl];
-                    }
-                    else
-                    {
-                        tex = new Texture2D(textureWidth, textureHeight);
-                        StartCoroutine(LoadTex(tex, texUrl));
-                        _textureCache.Add(texUrl, tex);
-                    }
-                    
-                    for(int i = 0; i < _walls.Length; ++i)
-                    {
-                        _walls[i].Renderer.material.mainTexture = tex;
-                    }
-
-                    break;
-                }
-
-                case "2":
-                {
-                    int textureWidth = 200;
-                    int textureHeight = 200;
-                    string texName = "test2.png";
-                    string texUrl = GetAssetFilePath(texName);
-
-                    Texture2D tex;
-                    if(_textureCache.ContainsKey(texUrl))
-                    {
-                        tex = _textureCache[texUrl];
-                    }
-                    else
-                    {
-                        tex = new Texture2D(textureWidth, textureHeight);
-                        StartCoroutine(LoadTex(tex, texUrl));
-                        _textureCache.Add(texUrl, tex);
-                    }
-
-                    for(int i = 0; i < _walls.Length; ++i)
-                    {
-                        _walls[i].Renderer.material.mainTexture = tex;
-                    }
-
-                    break;
-                }
-            }
-
-            _delta += (Time.deltaTime - _delta) * 0.1f;
-        }
-
         // TODO: Turn this into something more reasonable.
         IEnumerator LoadTex(Texture2D tex, string url)
         {
@@ -127,6 +67,72 @@ namespace RolePlayOverlord
                 yield return www;
                 www.LoadImageIntoTexture(tex);
             }
+        }
+
+        void Update()
+        {
+            if(isServer)
+            {
+                string keyString = Input.inputString;
+                switch(keyString)
+                {
+                    case "1":
+                    {
+                        int textureWidth = 200;
+                        int textureHeight = 200;
+                        string texName = "test.png";
+                        string texUrl = GetAssetFilePath(texName);
+
+                        Texture2D tex;
+                        if(_textureCache.ContainsKey(texUrl))
+                        {
+                            tex = _textureCache[texUrl];
+                        }
+                        else
+                        {
+                            tex = new Texture2D(textureWidth, textureHeight);
+                            StartCoroutine(LoadTex(tex, texUrl));
+                            _textureCache.Add(texUrl, tex);
+                        }
+
+                        for(int i = 0; i < _walls.Length; ++i)
+                        {
+                            _walls[i].Renderer.material.mainTexture = tex;
+                        }
+
+                        break;
+                    }
+
+                    case "2":
+                    {
+                        int textureWidth = 200;
+                        int textureHeight = 200;
+                        string texName = "test2.png";
+                        string texUrl = GetAssetFilePath(texName);
+
+                        Texture2D tex;
+                        if(_textureCache.ContainsKey(texUrl))
+                        {
+                            tex = _textureCache[texUrl];
+                        }
+                        else
+                        {
+                            tex = new Texture2D(textureWidth, textureHeight);
+                            StartCoroutine(LoadTex(tex, texUrl));
+                            _textureCache.Add(texUrl, tex);
+                        }
+
+                        for(int i = 0; i < _walls.Length; ++i)
+                        {
+                            _walls[i].Renderer.material.mainTexture = tex;
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            _delta += (Time.deltaTime - _delta) * 0.1f;
         }
 
         void OnGUI()
