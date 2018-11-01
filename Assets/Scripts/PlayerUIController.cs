a2f131b14b7636ca573b765caf516492b83afc8c
SpectatorQL <hlcombine@gmail.com>
2018-11-01 14:22:37 +0100

    Added the player UI to MainScene Fixed build order yet again...

---
+++ Assets/Scripts/PlayerUIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RolePlayOverlord
+{
+    public class PlayerUIController : MonoBehaviour
+    {
+        public bool IsHidden;
+
+        public void OnUIKeyPressed()
+        {
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            IsHidden = false;
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+            IsHidden = true;
+        }
+
+        public void Setup()
+        {
+            Show();
+        }
+    }
+}
