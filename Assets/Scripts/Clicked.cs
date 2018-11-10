a38a1dea396ad66cfff2fe78a5f588517075f10f
SpectatorQL <hlcombine@gmail.com>
2018-11-10 17:02:28 +0100

    Began work on GM UI scripting

---
+++ Assets/Scripts/Clicked.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RolePlayOverlord
+{
+    public class Clicked : MonoBehaviour
+    {
+        [SerializeField] HostUIController _globalController;
+        [SerializeField] GameObject _element;
+
+        public void ShowElement(int clientIndex)
+        {
+            _globalController.ShowMultiElement(_element, clientIndex);
+        }
+    }
+}
--- Assets/Scripts/HostUIController.cs
+++ Assets/Scripts/HostUIController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +7,29 @@ namespace RolePlayOverlord
 {
     public class HostUIController : MonoBehaviour
     {
-        public bool IsHidden;
+        [SerializeField] GameObject[] _multiElements = new GameObject[3];
+        GameObject _multiActiveElement;
 
+        bool _isHidden;
+
+        public void ShowMultiElement(GameObject elem, int clientIndex)
+        {
+            ShowMultiElement(elem);
+
+            var multiElem = elem.GetComponent<UIMultiElem>();
+            multiElem.MainTextSpace.text = clientIndex + "\nAnd some random giberrish LULZ";
+
+            // TODO: Call element-specific functions here.
+        }
+
+        public void ShowMultiElement(GameObject elem)
+        {
+            _multiActiveElement?.SetActive(false);
+            _multiActiveElement = elem;
+            _multiActiveElement.SetActive(true);
+        }
+
+        // TODO: GM UI doesn't need this function.
         public void OnUIKeyPressed()
         {
         }
@@ -15,17 +37,23 @@ namespace RolePlayOverlord
         public void Show()
         {
             gameObject.SetActive(true);
-            IsHidden = false;
+            _isHidden = false;
         }
 
         public void Hide()
         {
             gameObject.SetActive(false);
-            IsHidden = true;
+            _isHidden = true;
         }
 
         public void Setup()
         {
+            // TODO: Delete this guy once the UI is complete.
+            foreach(var e in _multiElements)
+            {
+                e.SetActive(false);
+            }
+
             Show();
         }
     }
