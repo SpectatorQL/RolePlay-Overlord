0dd26b58a7f44ad55ad1f3f2c4be861ea9ca6657
SpectatorQL <hlcombine@gmail.com>
2019-04-16 15:03:05 +0200

    Created a batch file for copying the default resources Reworked BuildTestAssets and renamed it to CreateTestMod CreateTestMod is no longer invoked post-build. It is now available under the "Overlord" tab in the editor

--- Assets/Editor/PostBuildEvents.cs
+++ Assets/Editor/PostBuildEvents.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.Callbacks;
-using RolePlayOverlord.Utils;
 using static RolePlayOverlord.IO;
 
 namespace RolePlayOverlord.Editor
@@ -19,42 +16,27 @@ namespace RolePlayOverlord.Editor
             appear in the console after building the player.
             IMPORTANT(SpectatorQL): This is non-portable!
         */
-        [PostProcessBuild(0)]
-        static void BuildTestAssets(BuildTarget target, string pathToBuiltProject)
+        [MenuItem("Overlord/Create default mod")]
+        static void CreateDefaultMod()
         {
-            // TODO: What is a resource path?
-            string[] defaultDataDirs =
+            var processInfo = new System.Diagnostics.ProcessStartInfo("copyTestResources.bat");
+            var process = System.Diagnostics.Process.Start(processInfo);
+            process.WaitForExit();
+
+
+            string[] defaultResourceDirs =
             {
-                "Mods/Default/Textures/Walls/",
-                "Mods/Default/Textures/Floor/",
-                "Mods/Default/Textures/Ceiling/",
-                "Mods/Default/Textures/Skybox/",
+                "Textures/Walls/",
+                "Textures/Floor/",
+                "Textures/Ceiling/",
+                "Textures/Skybox/",
 
-                "Mods/Default/Audio/",
+                "Audio/",
 
-                "Mods/Default/Models/Classes/",
-                "Mods/Default/Models/Figures/",
-
-                "Mods/Default/Documents/",
-
-                "Mods/Default/Saves/",
+                "Models/Classes/",
+                "Models/Figures/",
             };
-
-            // NOTE(SpectatorQL): Removes the built executable from the path.
-            int onePastLastSlash = pathToBuiltProject.OnePastLastSlash();
-            string buildDir = pathToBuiltProject.Remove(onePastLastSlash, pathToBuiltProject.Length - onePastLastSlash);
-            string modsDir = buildDir + "Mods/";
-
-            if(Directory.Exists(PATH(modsDir)))
-            {
-                Directory.Delete(PATH(modsDir), true);
-            }
-            foreach(var d in defaultDataDirs)
-            {
-                string path = PATH(buildDir + d);
-                Directory.CreateDirectory(path);
-                Debug.Assert(Directory.Exists(path));
-            }
+            System.Diagnostics.Debug.Assert(defaultResourceDirs.Length == (int)ResourceTypeID.Count);
 
 
             ModManifest defaultMod = new ModManifest
@@ -64,6 +46,8 @@ namespace RolePlayOverlord.Editor
                 Version = ModFormatInfo.VERSION
             };
 
+            ModData defaultModData = new ModData();
+
             int resourceTypeCount = (int)ResourceTypeID.Count;
             ResourceType[] resourceTypeEntries = new ResourceType[resourceTypeCount];
             for(int i = 0;
@@ -72,82 +56,44 @@ namespace RolePlayOverlord.Editor
             {
                 resourceTypeEntries[i] = new ResourceType();
             }
-            defaultMod.Data.ResourceTypeEntries = resourceTypeEntries;
+            defaultModData.ResourceTypeEntries = resourceTypeEntries;
 
-            List<ResourceType> resourceList = new List<ResourceType>();
-            WriteResources(resourceList, ResourceTypeID.WallTexture, defaultDataDirs[0], buildDir);
-            WriteResources(resourceList, ResourceTypeID.FloorTexture, defaultDataDirs[1], buildDir);
-            WriteResources(resourceList, ResourceTypeID.CeilingTexture, defaultDataDirs[2], buildDir);
-            WriteResources(resourceList, ResourceTypeID.SkyboxTexture, defaultDataDirs[3], buildDir);
-            WriteResources(resourceList, ResourceTypeID.Audio, defaultDataDirs[4], buildDir);
-            WriteResources(resourceList, ResourceTypeID.CharacterModel, defaultDataDirs[5], buildDir);
-            WriteResources(resourceList, ResourceTypeID.FigureModel, defaultDataDirs[6], buildDir);
-            defaultMod.Data.Resources = resourceList.ToArray();
-            CopyResources(defaultDataDirs[7], buildDir);
-            CopyResources(defaultDataDirs[8], buildDir);
+            string workingDirectory = PATH("build/Mods/Default/");
+            int runningResourceCount = 0;
+            List<Resource> resourcesToBuild = new List<Resource>();
+            for(int i = 0;
+                i < resourceTypeCount;
+                ++i)
+            {
+                string resDir = PATH(workingDirectory + defaultResourceDirs[i]);
+                string[] resourceFiles = Directory.GetFiles(resDir);
+                Resource[] resources = new Resource[resourceFiles.Length];
+                for(int j = 0;
+                    j < resourceFiles.Length;
+                    ++j)
+                {
+                    resources[j] = new Resource { File = resourceFiles[j] };
+                }
 
+                defaultModData.ResourceTypeEntries[i].FirstResourceIndex = runningResourceCount;
+                for(int j = 0;
+                    j < resources.Length;
+                    ++j)
+                {
+                    resourcesToBuild.Add(resources[j]);
+                    ++runningResourceCount;
+                }
+                defaultModData.ResourceTypeEntries[i].OnePastLastResourceIndex = runningResourceCount;
+            }
+            defaultModData.Resources = resourcesToBuild.ToArray();
 
-            string defaultModManifest = "build/Mods/Default/Default.rmm";
-            using(var fs = new FileStream(PATH(defaultModManifest), FileMode.Create, FileAccess.Write, FileShare.None))
+            defaultMod.Data = defaultModData;
+
+            string defaultModManifestPath = PATH("build/Mods/Default/Default.rmm");
+            using(var fs = new FileStream(defaultModManifestPath, FileMode.Create, FileAccess.Write, FileShare.None))
             {
                 var formatter = new BinaryFormatter();
                 formatter.Serialize(fs, defaultMod);
-            }
-        }
-
-        static void WriteResources(List<ResourceType> resList, ResourceTypeID resType, string resTypeDir, string buildDir)
-        {
-            string dir = "Assets/" + resTypeDir;
-            string[] files = Directory.GetFiles(PATH(dir))
-                .Where(file =>
-                {
-                    return EXTENSION(file) != ".meta";
-                })
-                .ToArray();
-
-            ResourceType res = new ResourceType()
-            {
-                ID = resType,
-                Data = new string[files.Length]
-            };
-
-            for(int i = 0;
-                i < files.Length;
-                ++i)
-            {
-                string assetFile = Path.GetFileName(files[i]);
-                string srcFile = dir + assetFile;
-                string destFile = buildDir + resTypeDir + assetFile;
-
-                File.Copy(PATH(srcFile), PATH(destFile), true);
-                UnityEngine.Debug.Assert(File.Exists(PATH(destFile)));
-
-                res.Data[i] = resTypeDir + assetFile;
-            }
-
-            resList.Add(res);
-        }
-
-        static void CopyResources(string resTypeDir, string buildDir)
-        {
-            string dir = "Assets/" + resTypeDir;
-            string[] files = Directory.GetFiles(PATH(dir))
-                .Where(file =>
-                {
-                    return EXTENSION(file) != ".meta";
-                })
-                .ToArray();
-
-            for(int i = 0;
-                i < files.Length;
-                ++i)
-            {
-                string assetFile = Path.GetFileName(files[i]);
-                string srcFile = dir + assetFile;
-                string destFile = buildDir + resTypeDir + assetFile;
-
-                File.Copy(PATH(srcFile), PATH(destFile), true);
-                UnityEngine.Debug.Assert(File.Exists(PATH(destFile)));
             }
         }
     }
